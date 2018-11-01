235665484ca3cb6cde49872e80053214fb23572f
Cole Henley <cole@ournameismud.co.uk>
2018-11-01 10:14:06 +0000

    fix video issue (image fallback)

--- src/Buggles.php
+++ src/Buggles.php
@@ -49,7 +49,7 @@ class Buggles extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '0.0.1';
+    public $schemaVersion = '0.0.2';
 
     // Public Methods
     // =========================================================================
